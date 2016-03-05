aea83513b27ad26d495486940b27837ef242e8dc
BreakVoid <breakvoid@hotmail.com>
2016-03-05 19:23:27 +0800

    map.hpp filled

--- include/map.hpp
+++ include/map.hpp
@@ -1,3 +1,143 @@
+/**
+ * implement a container like std::vector
+ */
 #ifndef SJTU_MAP_HPP
 #define SJTU_MAP_HPP
+
+// only for std::less<T>
+#include <functional>
+#include <cstddef>
+
+namespace sjtu {
+
+template<class FT, class ST>
+class pair {
+public:
+	FT first;
+	ST second;
+};
+
+template<
+	class Key,
+	class T,
+	class Compare = std::less<Key>
+> class map {
+	/**
+	 * the internal type of data.
+	 * it should have a default constructor, a copy constructor.
+	 */
+	class value_type {
+
+	};
+	/**
+	 * see BidirectionalIterator for help.
+	 */
+	class iterator {
+	private:
+		/**
+		 * TODO add data members
+		 *   just add whatever you want.
+		 */
+	public:
+		/**
+		 * return a new iterator which pointer n-next elements
+		 *   even if there are not enough elements, just return the answer.
+		 * as well as operator-
+		 */
+		/**
+		 * TODO iter++
+		 */
+		iterator operator++(int) {}
+		/**
+		 * TODO ++iter
+		 */
+		iterator& operator++() {}
+		/**
+		 * TODO iter--
+		 */
+		iterator operator--(int) {}
+		/**
+		 * TODO --iter
+		 */
+		iterator& operator--() {}
+		/**
+		 * TODO *it
+		 */
+		T& operator*() const{}
+	};
+	class const_iterator {
+
+	};
+public:
+	/**
+	 * TODO two constructors
+	 */
+	map() {}
+	map(const map<Key, T, Compare> &other) {}
+	/**
+	 * TODO
+	 * access specified element with bounds checking
+	 * Returns a reference to the mapped value of the element with key equivalent to key.
+	 * If no such element exists, an exception of type `index_out_of_bound'
+	 */
+	T & at(const Key &key) {}
+	const T & at(const Key &key) const {}
+	/**
+	 * TODO
+	 * access specified element 
+	 * Returns a reference to the value that is mapped to a key equivalent to key,
+	 *   performing an insertion if such key does not already exist.
+	 */
+	T & operator[](const Key &key) {}
+	/**
+	 * return a iterator to the beginning
+	 */
+	iterator begin() {}
+	const_iterator cbegin() const {}
+	/**
+	 * return a iterator to the end
+	 * in fact, it returns past-the-end.
+	 */
+	iterator end() {}
+	const_iterator cend() const {}
+	/**
+	 * checks whether the container is empty
+	 * return true if empty, otherwise false.
+	 */
+	bool empty() const {}
+	/**
+	 * returns the number of elements.
+	 */
+	size_t size() const {}
+	/**
+	 * clears the contents
+	 */
+	void clear() {}
+	/**
+	 * insert an element.
+	 * return a pair, the first of the pair is
+	 *   the iterator to the new element, 
+	 *   the second one is true if insert successfully, or false.
+	 */
+	pair<iterator, bool> insert(const value_type &value) {}
+	/**
+	 * Returns the number of elements with key 
+	 *   that compares equivalent to the specified argument,
+	 *   which is either 1 or 0 
+	 *     since this container does not allow duplicates.
+	 * The default method of check the equivalence is !(a < b || b > a)
+	 */
+	size_t count() {}
+	/**
+	 * Finds an element with key equivalent to key.
+	 * key value of the element to search for.
+	 * Iterator to an element with key equivalent to key.
+	 *   If no such element is found, past-the-end (see end()) iterator is returned.
+	 */
+	iterator find(const Key &key) {}
+	const_iterator find(const Key &key) const;
+};
+
+}
+
 #endif
