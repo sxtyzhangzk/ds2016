d9252d48d9fb0b345881d192a9c8276d09ed1895
BreakVoid <breakvoid@hotmail.com>
2016-03-06 17:07:26 +0800

    + algorithm.hpp & list.hpp (not completed)

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -1,3 +1,29 @@
 #ifndef SJTU_ALGORITHM_HPP
 #define SJTU_ALGORITHM_HPP
+
+#include "vector.hpp"
+#include "list.hpp"
+
+namespace sjtu {
+
+template<class VecIter>
+void sort(VecIter beg, VecIter end) {}
+
+template<class VecIter, class Compare>
+void sort(VecIter beg, VecIter end, Compare comp) {}
+
+template<class ListIter>
+void list_sort(ListIter beg, ListIter end) {}
+
+template<class ListIter, class Compare>
+void list_sort(ListIter beg, ListIter end, Compare comp) {}
+
+template<class VecIter>
+void make_heap(VecIter beg, VecIter end) {}
+
+template<class VecIter, class Compare>
+void make_heap(VecIter beg, VecIter end, Compare comp) {}
+
+}
+
 #endif
--- include/list.hpp
+++ include/list.hpp
@@ -7,7 +7,13 @@ namespace sjtu {
 
 template<typename T>
 class list {
+	class iterator {
 
+	};
+	class const_iterator {
+
+	};
+public:
 };
 
 }
