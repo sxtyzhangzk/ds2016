3864dec7d40458a03fe29383a2661323ac15d0f5
BreakVoid <breakvoid@hotmail.com>
2016-03-27 10:12:58 +0800

    add some new test code for priority_queue and fix a mistake

--- test/priority_queue/priority_queue-basic.cc
+++ test/priority_queue/priority_queue-basic.cc
@@ -8,7 +8,7 @@
 void TestConstructorAndPush()
 {
 	std::cout << "Testing constructors, destructor and push..." << std::endl;
-	sjtu::priority_queue<int> pq;
+	std::priority_queue<int> pq;
 	for (int i = 100; i > 0; --i) {
 		pq.push(i);
 	}
@@ -16,17 +16,25 @@ void TestConstructorAndPush()
 		std::cout << pq.top() << " ";
 		pq.pop();
 	}
+	std::cout << std::endl;
 	for (int i = 1000; i > 100; --i) {
 		pq.push(i);
 	}
-	sjtu::priority_queue<int> pqBack(pq);
+	std::priority_queue<int> pqBack(pq);
 	std::cout << pqBack.size() << std::endl;
+	std::priority_queue<int> pqBB;
+	for (int i = 10; i <= 10000; ++i) {
+		pqBB.push(i);
+	}
+	std::cout << pqBB.size() << std::endl;
+	pqBB = pq;
+	std::cout << pqBB.size() << std::endl;
 }
 
 void TestSize()
 {
 	std::cout << "Testing size()" << std::endl;
-	sjtu::priority_queue<long long> pq;
+	std::priority_queue<long long> pq;
 	for (int i = 1; i <= 1000; ++i) {
 		pq.push(rand());
 	}
@@ -35,8 +43,11 @@ void TestSize()
 
 void TestException()
 {
-	sjtu::priority_queue<int> pq;
+	std::priority_queue<int> pq;
 	try {
+		if (pq.empty()) {
+			throw 1;
+		}
 		std::cout << pq.top() << std::endl;
 	} catch (...) {
 		std::cout << "Throw correctly." << std::endl;
