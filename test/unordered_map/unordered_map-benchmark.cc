6acd57986ef44463574a1534050eee69f3323da0
BreakVoid <breakvoid@hotmail.com>
2016-05-06 17:41:17 +0800

    upload unordered_map test case

---
+++ test/unordered_map/unordered_map-benchmark.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <chrono>
+#include <ctime>
+#include "../../include/unordered_map.hpp"
+#include "../common/class-bint.hpp"
+
+
+std::default_random_engine randomEngine((unsigned)time(NULL));
+
+std::string RandomBint()
+{
+	std::uniform_int_distribution<int> lengthGenerator(8, 10);
+	std::uniform_int_distribution<char> digitGenerator('0', '9');
+	std::uniform_int_distribution<char> firstDigitGenerator('1', '9');
+	int len = lengthGenerator(randomEngine);
+	std::string res(firstDigitGenerator(randomEngine), 1);
+	for (int i = 1; i < len; ++i) {
+		res += digitGenerator(randomEngine);
+	}
+	return res;
+}
+
+bool Benchmark()
+{
+	std::unordered_map<std::string, Util::Bint> stdBintHashMap;
+	sjtu::unordered_map<std::string, Util::Bint> myBintHashMap;
+	for (int t = 0; t < 500000; ++t) {
+		std::string integer = RandomBint();
+		stdBintHashMap.insert(std::make_pair(integer, Util::Bint(integer)));
+		myBintHashMap.insert(sjtu::pair<std::string, Util::Bint>(integer, Util::Bint(integer)));
+	}
+	for (int t = 0; t < 500000; ++t) {
+		std::string integer = RandomBint();
+		Util::Bint inStdMap = stdBintHashMap[integer];
+		Util::Bint inMyMap = myBintHashMap[integer];
+	}
+	stdBintHashMap.clear();
+	myBintHashMap.clear();
+	for (int t = 0; t < 30000000; ++t) {
+		std::string integer = RandomBint();
+		stdBintHashMap.insert(std::make_pair(integer, Util::Bint(integer)));
+		myBintHashMap.insert(sjtu::pair<std::string, Util::Bint>(integer, Util::Bint(integer)));
+	}
+	return stdBintHashMap.size() == myBintHashMap.size();
+}
+
+int main()
+{
+	std::cout << (Benchmark() ? "Benchmark passed" : "Benchmark failed") << std::endl;
+	return 0;
+}
