f456c591135a162f3c479ef6f4dd7efe5484bb88
BreakVoid <breakvoid@hotmail.com>
2016-05-06 17:48:44 +0800

    update the include path in unordered_map-benchmark.cc

--- test/unordered_map/unordered_map-benchmark.cc
+++ test/unordered_map/unordered_map-benchmark.cc
@@ -5,8 +5,8 @@
 #include <unordered_map>
 #include <chrono>
 #include <ctime>
-#include "../../include/unordered_map.hpp"
-#include "../common/class-bint.hpp"
+#include "unordered_map.hpp"
+#include "class-bint.hpp"
 
 
 std::default_random_engine randomEngine((unsigned)time(NULL));
@@ -28,12 +28,12 @@ bool Benchmark()
 {
 	std::unordered_map<std::string, Util::Bint> stdBintHashMap;
 	sjtu::unordered_map<std::string, Util::Bint> myBintHashMap;
-	for (int t = 0; t < 500000; ++t) {
+	for (int t = 0; t < 800000; ++t) {
 		std::string integer = RandomBint();
 		stdBintHashMap.insert(std::make_pair(integer, Util::Bint(integer)));
 		myBintHashMap.insert(sjtu::pair<std::string, Util::Bint>(integer, Util::Bint(integer)));
 	}
-	for (int t = 0; t < 500000; ++t) {
+	for (int t = 0; t < 1000000; ++t) {
 		std::string integer = RandomBint();
 		Util::Bint inStdMap = stdBintHashMap[integer];
 		Util::Bint inMyMap = myBintHashMap[integer];
